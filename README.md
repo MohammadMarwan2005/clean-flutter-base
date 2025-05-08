984b69666735b7ae408c9441ca5a75f692f68e71
Mohammad Al-Aishat <alaishatmohammad@gmail.com>
2025-05-08 13:00:02 +0300

    👉 Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clean_flutter_base
+
+A clean flutter template to use later
---
+++ android/app/src/main/kotlin/com/example/clean_flutter_base/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clean_flutter_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
